9311f258ffc782266df8f723c3c0b4b1b3c7ca6e
MakandarSahil <sahilmakandar15@gmail.com>
2025-07-02 15:04:14 +0000

    iSort

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -7,14 +7,27 @@ public class InsertionSort {
 
     public static void iSort(int[] arr){
         int n = arr.length;
-        for (int i = 0; i < n-1; i ++){
+        for (int i = 1; i < n; i ++){
             int j = i;
             while(j > 0 && arr[j - 1] > arr[j]){
                 int temp = arr[j];
                 arr[j] = arr[j-1];
                 arr[j-1] = temp;
+                j--;
             }
         }
+    }
+
+    public static void RecursiveIsort(int[] arr,int i, int n){
+        if (i == n) return;
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]){
+            int temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j--;
+        }
+        RecursiveIsort(arr, i+1, n);
     }
     public static void main(String[] args){
         System.out.println("Insertion Sort !!");
@@ -25,6 +38,10 @@ public class InsertionSort {
 
         iSort(arr);
         System.out.println("After Insertion Sort : ");
+        ArrayUtils.printArr(arr);
+
+        RecursiveIsort(arr, 0, arr.length-1);
+        System.out.println("After Recusrsive Insertion Sort : ");
         ArrayUtils.printArr(arr);
     }
 }
